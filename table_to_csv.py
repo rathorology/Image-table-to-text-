be1a170278c20e1ddc1a10a39fd68fe8aa023a16
rathorology <you@example.com>
2020-06-28 04:29:56 +0530

    table without vertical line to csv

--- table_to_csv.py
+++ table_to_csv.py
@@ -6,76 +6,86 @@ import random
 import kraken as k
 import warnings
 from kraken.lib.models import load_any
-from kraken import rpred, binarization, pageseg
+from kraken import rpred, binarization
 from PIL import Image
 from subprocess import call
 
 warnings.filterwarnings("ignore", category=FutureWarning)
 
-##Kraken Model Loading
+## ---Loading Kraken Model---
 model = load_any("en-default.mlmodel")
 
-img = cv2.imread('image_tables/ROI_0.png')
+img = cv2.imread('images/tabualr.png')
+
+## ---Binarization of image---
 genrator_image = Image.fromarray(img)
 genrator_image = binarization.nlbin(genrator_image)
 
+# ----Grayscaling Image----
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # --- performing Otsu threshold ---
 ret, thresh1 = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
+cv2.imwrite("processed_image/threshold.png", thresh1)
 # cv2.imshow('thresh1', thresh1)
 # cv2.waitKey(0)
 
-
+# ----Image dialation----
 rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (15, 3))
 dilation = cv2.dilate(thresh1, rect_kernel, iterations=1)
+cv2.imwrite("processed_image/dilation.png", dilation)
 # cv2.imshow('dilation', dilation)
 # cv2.waitKey(0)
 
 
 # ---Finding contours ---
 contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-box_list = []
 
+dummy_image = img.copy()
 row_list = list()
-i = 0
-one_row = list()
+old_y = 0
+single_row = list()
 for idx, cnt in enumerate(contours[::-1]):
-    x, y, w, h = cv2.boundingRect(cnt)
-    box_list.append((x, y, x + w, y + h))
-    if idx == 0:
-        one_row.append([x, y, w, h])
-    else:
-        if y - i < 15:
-            one_row.append([x, y, w, h])
+    area = cv2.contourArea(cnt)
+
+    if area < 25000:
+        x, y, w, h = cv2.boundingRect(cnt)
+
+        ## Different color for each row
+        r = random.randint(0, 255)
+        g = random.randint(0, 255)
+        b = random.randint(0, 255)
+
+        # Drawing box
+        cv2.rectangle(dummy_image, (x, y), (x + w, y + h), (b, g, r), 2)
+        if idx == 0:
+            single_row.append([x, y, w, h])
         else:
-            row_list.append(one_row)
-            one_row = list()
-            one_row.append([x, y, w, h])
-    i = y
-# print(row_list)
-color = (np.random.random(size=3) * 256)
-im2 = img.copy()
+            if y - old_y < 5:
+                single_row.append([x, y, w, h])
+            else:
+                row_list.append(single_row)
+                single_row = list()
+                single_row.append([x, y, w, h])
+        old_y = y
+
+cv2.imwrite("processed_image/show_box.png", dummy_image)
+# cv2.imshow('final', dummy_image)
+# cv2.waitKey(0)
+
+
+# color = (np.random.random(size=3) * 256)
+
 all_text = list()
-count_in_each_row = list()
-
-
-def most_common(lst):
-    return max(set(lst), key=lst.count)
-
+row_count = list()
 
 for row_boxes in row_list:
-
-    r = random.randint(0, 255)
-    g = random.randint(0, 255)
-    b = random.randint(0, 255)
     row_text = list()
     for one_box in row_boxes:
         x, y, w, h = one_box
-        cv2.rectangle(im2, (x, y), (x + w, y + h), (b, g, r), 3)
+        #########################################################################################
 
-        # small = img[y:y + h, x:x + w]
-        # box_text = pytesseract.image_to_string(small, lang='eng')
+        ## Kraken Text Extraction
         cord = [x, y, x + w, y + h]
         bound = {'boxes': [tuple(cord)], 'text_direction': 'horizontal-lr'}
         ##Use this for using Kraken API , uncomment model from above
@@ -83,6 +93,10 @@ for row_boxes in row_list:
         nxt_gen = next(generator)
         box_text = nxt_gen.prediction
 
+        ##########################################################################################
+
+        ## Kraken bash script
+        # small = img[y:y + h, x:x + w]
         # cv2.imwrite("images/temp.jpg", small)
         # box_text = " "
         # try:
@@ -91,29 +105,23 @@ for row_boxes in row_list:
         # except Exception as e:
         #     pass
 
-        if box_text == '':
-            box_text = " "
         row_text.append(box_text)
         print("Box_Text = {} | Y = {}".format(box_text, y))
     print(row_text)
-    count_in_each_row.append(len(row_text))
+    row_count.append(len(row_text))
     all_text.append(row_text)
     print("======================================================================")
 print(all_text)
 updated_text_rows = list()
 
-columns = most_common(count_in_each_row)
-# Creating a dataframe of the generated OCR list
+columns = max(set(row_count), key=row_count.count)
+
 for rows in all_text:
     diff = columns - len(rows)
     rows = rows + [" "] * diff
     updated_text_rows.append(rows)
 
+# Creating a dataframe of the generated OCR list
 arr = np.array(updated_text_rows)
 dataframe = pd.DataFrame(arr, columns=range(0, columns))
-dataframe.to_csv("csv/output.csv", index=False)
-
-cv2.imshow('final', im2)
-cv2.waitKey(0)
-# print(len(box))
-# print(len(text_list))
+dataframe.to_csv("output_csv/output.csv", index=False)
